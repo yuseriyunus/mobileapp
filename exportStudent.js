70615bc136f56d840838af3197de6e078115820a
yuseriyunus <44749573+yuseriyunus@users.noreply.github.com>
2020-03-13 17:07:58 +0800

    Update exportStudent.js

--- exportStudent.js
+++ exportStudent.js
@@ -18,6 +18,7 @@ class Student {
         console.log(`Name: ${this.name}`);
         console.log(`Matric Number: ${this.matricNo}`);
         console.log(`Major: ${this.major}`);
+        process.exit();
     }
 }
 
