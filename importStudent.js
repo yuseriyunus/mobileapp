3f08a60805b0e2a19c4568e2afabb8db6c4a4ba6
yuseriyunus <44749573+yuseriyunus@users.noreply.github.com>
2020-03-13 17:07:35 +0800

    Update importStudent.js

--- importStudent.js
+++ importStudent.js
@@ -14,7 +14,6 @@ getDetails();
 
   
 async function getDetails() {
-//may also use callback to iterate the 3 question
     name = await ask('What is your name? ');
     matricNo = await ask('What is your matric number? ');
     major = await ask('What is your major? ');
